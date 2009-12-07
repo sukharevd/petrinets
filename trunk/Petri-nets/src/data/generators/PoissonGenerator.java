70667c2c36072ed49aa36740b271290d5d7c9f69
vixentael@gmail.com <vixentael@gmail.com@50f4c8c0-c7f2-11de-a9c8-2fbcfba63733>
2009-12-07 21:13:41 +0000

    Changed all generators..

--- trunk/Petri-nets/src/data/generators/PoissonGenerator.java
+++ trunk/Petri-nets/src/data/generators/PoissonGenerator.java
@@ -11,7 +11,8 @@ public class PoissonGenerator implements Generator {
 
     private double lyamda;
 
-    private double b = 0.;
+    private double b = 0.0;
+    private Generator kGen;
 
     public PoissonGenerator() {
         this.lkgValues = null;
@@ -23,10 +24,11 @@ public class PoissonGenerator implements Generator {
         double c = Math.pow(3, 3);
         double d = Math.pow(2, 32);
         double w0 = 428.0;
-        int quantity = 10000; // TODO: 3000?
-        Generator kGen = new LCGenerator(a, c, d, w0);
-        this.lkgValues = kGen.generateList(quantity);
+        //int quantity = 10000; // TODO: 3000?
+        this.kGen = new LCGenerator(a, c, d, w0);
+        //this.lkgValues = kGen.generateList(quantity);
 
+        //this.lkgValues = kGen.generateList();
     }
 
     public PoissonGenerator(ArrayList<Double> newvals, double lyamda) {
@@ -36,29 +38,52 @@ public class PoissonGenerator implements Generator {
         this.values = new ArrayList<Double>();
     }
 
-    @Override
-    public ArrayList<Double> generateList(int quantity) {
-        for (; curIndex < quantity; curIndex++) {
-            values.add(curIndex, -Math.log(1.0 - lkgValues.get(curIndex))
-                    / lyamda);
-            if (values.get(curIndex) > b) {
-                b = values.get(curIndex);
-            }
-        }
+//    @Override
+//    public ArrayList<Double> generateList(int quantity) {
+//        for (; curIndex < quantity; curIndex++) {
+//            values.add(curIndex, -Math.log(1.0 - lkgValues.get(curIndex))
+//                    / lyamda);
+//            if (values.get(curIndex) > b) {
+//                b = values.get(curIndex);
+//            }
+//        }
+//
+//        return values;
+//    }
 
-        return values;
-    }
-
+//    @Override
+//    public ArrayList<Double> generateList() {
+//    	values.add(0, -Math.log(1.0 - lkgValues.get(0))
+//                / lyamda);
+//        if (values.get(0) > b) {
+//            b = values.get(0);
+//        }
+//
+//        return values;
+//    }
+    
+//    @Override
+//    public Double generateValue() {
+//        values.add(curIndex, -Math.log(1.0 - lkgValues.get(curIndex)) / lyamda);
+//        if (values.get(curIndex) > b) {
+//            b = values.get(curIndex);
+//        }
+//
+//        Double res = values.get(curIndex);
+//        curIndex++;
+//
+//        return res;
+//    }
+    
     @Override
     public Double generateValue() {
-        values.add(curIndex, -Math.log(1.0 - lkgValues.get(curIndex)) / lyamda);
-        if (values.get(curIndex) > b) {
-            b = values.get(curIndex);
+    	values.add(0, -Math.log(1.0 - this.kGen.generateValue())
+                / lyamda);
+        if (values.get(0) > b) {
+            b = values.get(0);
         }
 
-        Double res = values.get(curIndex);
-        curIndex++;
-
+        Double res = values.get(0);
         return res;
     }
 
