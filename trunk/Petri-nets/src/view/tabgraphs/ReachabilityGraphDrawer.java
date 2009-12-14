559e985adbb48d2ce70a9fb4f96cce524081b757
vixentael@gmail.com <vixentael@gmail.com@50f4c8c0-c7f2-11de-a9c8-2fbcfba63733>
2009-12-14 21:46:28 +0000

    gradients [54 revision] made authors checked

--- trunk/Petri-nets/src/view/tabgraphs/ReachabilityGraphDrawer.java
+++ trunk/Petri-nets/src/view/tabgraphs/ReachabilityGraphDrawer.java
@@ -17,7 +17,6 @@ import data.TreeofPetriNet;
 
 /**
  * @author <a href="mailto:jacky@gmail.com">Dzyuban Yuriy</a>
- * @author <a href="mailto:vixentael@gmail.com">Voitova Anastasi</a>
  * 
  */
 public class ReachabilityGraphDrawer extends JPanel implements Scalable {
